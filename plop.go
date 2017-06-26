b99ded6221c4b9a544d8642159f02b282a6ab850
Golga <golgarud@live.fr>
2017-06-26 15:09:50 +0200

    CORE: basic ping from array

---
+++ plop.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"net"
+)
+
+func main() {
+	fmt.Println("!selpmaxe oG ,olleH")
+	domainList := [...] string {
+		"bulko.net",
+		"koregraf.com",
+		"golga.io"	}
+
+	for i := 0; i < len(domainList); i++ {
+		fmt.Println(domainList[i])
+		addr, err := net.LookupHost(domainList[i])
+		fmt.Println(addr, err)
+	}
+}
